5a9fae6989c37336408e06095f8196ef6a8ee56c
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-01-05 14:57:02 -0300

    docs: correção de docs

--- src/br/com/caio/streams/teste/TesteStream03.java
+++ src/br/com/caio/streams/teste/TesteStream03.java
@@ -3,23 +3,28 @@ package br.com.caio.streams.teste;
 import br.com.caio.streams.dominio.Animes;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TesteStream03 {
 
-    private static List<Animes> animesList = new ArrayList<>(List.of(
-            new Animes("Naruto", 10),
-            new Animes("DeathNoth", 5.50),
-            new Animes("Attack on titan", 8.25),
-            new Animes("Full Metal Aquimista", 3.99),
-            new Animes("HunterxHunter", 2.50),
-            new Animes("Dragon ball Z", 10.50)
-    ));
-
     public static void main(String[] args) {
 
+    List<List<String>> jogadores = new ArrayList<>();
+    List<String> jogadoresFluminense = new ArrayList<>(List.of("Samuel Xavierr", "Martinelli", "Ganso", "Fabio", "Cano"));
+    List<String> jogadoresFlamengo = new ArrayList<>(List.of("Arrascaeta", "Jorginho", "Plata"));
+    List<String> jogadoresBotafogo = new ArrayList<>(List.of("Barboza", "Savarino", "Alex Telles"));
+    List<String> jogadoresVasco = new ArrayList<>(List.of("Coutinho"));
 
+    jogadores.add(jogadoresFluminense);
+    jogadores.add(jogadoresFlamengo);
+    jogadores.add(jogadoresBotafogo);
+    jogadores.add(jogadoresVasco);
+
+    jogadores.stream()
+            .flatMap(Collection::stream)
+            .forEach(System.out::println);
 }
 }
