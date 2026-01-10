9e4545c97aad184946301167ec80a42d92440bee
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-01-10 17:06:53 -0300

    feat: continuando streams

--- src/br/com/caio/streams/dominio/Animes.java
+++ src/br/com/caio/streams/dominio/Animes.java
@@ -3,10 +3,17 @@ package br.com.caio.streams.dominio;
 public class Animes {
    private String titulo;
    private double preco;
+   private Categoria categoria;
 
     public Animes(String titulo, double preco) {
         this.titulo = titulo;
         this.preco = preco;
+    }
+
+    public Animes(String titulo, double preco, Categoria categoria) {
+        this.titulo = titulo;
+        this.preco = preco;
+        this.categoria = categoria;
     }
 
     public String getTitulo() {
@@ -25,11 +32,16 @@ public class Animes {
         this.preco = preco;
     }
 
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
     @Override
     public String toString() {
         return "Animes{" +
                 "titulo='" + titulo + '\'' +
                 ", preco=" + preco +
+                ", categoria=" + categoria +
                 '}';
     }
 }
---
+++ src/br/com/caio/streams/teste/CollectStream/TesteStream11.java
@@ -0,0 +1,52 @@
+package br.com.caio.streams.teste.CollectStream;
+
+import br.com.caio.streams.dominio.Animes;
+import br.com.caio.streams.dominio.Categoria;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TesteStream11 {
+    private static List<Animes> animesList = new ArrayList<>(List.of(
+            new Animes("Naruto", 10, Categoria.ROMANCE),
+            new Animes("DeathNoth", 5.50, Categoria.DRAMA),
+            new Animes("Attack on titan", 8.25, Categoria.FANTASY),
+            new Animes("Full Metal Aquimista", 3.99, Categoria.ROMANCE),
+            new Animes("HunterxHunter", 2.50, Categoria.FANTASY),
+            new Animes("Dragon ball Z", 10.50, Categoria.DRAMA)
+    ));
+
+
+    public static void main(String[] args) {
+
+        Map<Categoria, List<Animes>> categoriaListMap = new HashMap<>();
+        List<Animes> romance = new ArrayList<>();
+        List<Animes> drama = new ArrayList<>();
+        List<Animes> fantasia = new ArrayList<>();
+
+        for (Animes animes : animesList) {
+           switch (animes.getCategoria()){
+               case DRAMA: drama.add(animes);
+               break;
+               case FANTASY: fantasia.add(animes);
+               break;
+               case ROMANCE: romance.add(animes);
+               break;
+           }
+        }
+
+        categoriaListMap.put(Categoria.DRAMA, drama);
+        categoriaListMap.put(Categoria.FANTASY, fantasia);
+        categoriaListMap.put(Categoria.ROMANCE, romance);
+
+        System.out.println(categoriaListMap);
+
+        Map<Categoria, List<Animes>> collect = animesList.stream().collect(Collectors.groupingBy(Animes::getCategoria));
+        System.out.println(collect);
+
+    }
+    }
+
